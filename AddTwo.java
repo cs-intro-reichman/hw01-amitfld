1a2ccb6065d1ad381a76b40a123d33d3e4e8a14e
amitfld <amit.feldman@post.runi.ac.il>
2023-12-21 13:13:16 +0200

    Update AddTwo.java

--- AddTwo.java
+++ AddTwo.java
@@ -1,8 +1,16 @@
 /*
- * Adds two given integers and prints the result in a fancy way.
- */
+Write a program (AddTwo.java) that adds two given integers and prints the result in a fancy way.
+The command line is: java AddTwo a b.
+*/
 public class AddTwo {
-	public static void main(String[] args) {
-		// Put your code here	
-	}
+    public static void main(String[] args) {
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+
+        int sum = num1 + num2;
+
+        String output = String.format("%d + %d = %d", num1, num2, sum);
+        System.out.println(output);
+    }
+
 }
